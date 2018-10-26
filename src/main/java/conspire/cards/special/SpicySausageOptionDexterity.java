a452931b569fa93778fe9caaac29f4ff8aa0a1ed
Twan van Laarhoven <twanvl@gmail.com>
2018-10-26 21:57:12 +0200

    Spicy Sausage options

---
+++ src/main/java/conspire/cards/special/SpicySausageOptionDexterity.java
@@ -0,0 +1,40 @@
+package conspire.cards.special;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+
+import basemod.abstracts.CustomCard;
+import conspire.Conspire;
+
+public class SpicySausageOptionDexterity extends CustomCard {
+    public static final String ID = "conspire:SpicySausageOptionDexterity";
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    private static final int COST = -2;
+
+    public SpicySausageOptionDexterity(int amount) {
+        super(ID, NAME, Conspire.cardImage(ID), COST, DESCRIPTION, CardType.SKILL, CardColor.COLORLESS, CardRarity.SPECIAL, CardTarget.NONE);
+        this.magicNumber = this.baseMagicNumber = amount;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(p, p, new DexterityPower(p, this.magicNumber), this.magicNumber));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new SpicySausageOptionDexterity(this.baseMagicNumber);
+    }
+
+    @Override
+    public void upgrade() {
+    }
+}
---
+++ src/main/java/conspire/cards/special/SpicySausageOptionStrength.java
@@ -0,0 +1,40 @@
+package conspire.cards.special;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+
+import basemod.abstracts.CustomCard;
+import conspire.Conspire;
+
+public class SpicySausageOptionStrength extends CustomCard {
+    public static final String ID = "conspire:SpicySausageOptionStrength";
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    private static final int COST = -2;
+
+    public SpicySausageOptionStrength(int amount) {
+        super(ID, NAME, Conspire.cardImage(ID), COST, DESCRIPTION, CardType.SKILL, CardColor.COLORLESS, CardRarity.SPECIAL, CardTarget.NONE);
+        this.magicNumber = this.baseMagicNumber = amount;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(p, p, new StrengthPower(p, this.magicNumber), this.magicNumber));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new SpicySausageOptionStrength(this.baseMagicNumber);
+    }
+
+    @Override
+    public void upgrade() {
+    }
+}
