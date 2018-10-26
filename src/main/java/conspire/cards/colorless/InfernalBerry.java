ad2776237bf244239a42d1d9b257ca2a15e18f8f
Twan van Laarhoven <twanvl@gmail.com>
2018-10-26 22:20:44 +0200

    (cleanup)

--- src/main/java/conspire/cards/colorless/InfernalBerry.java
+++ src/main/java/conspire/cards/colorless/InfernalBerry.java
@@ -1,14 +1,11 @@
 package conspire.cards.colorless;
 
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.EvolvePower;
-import com.megacrit.cardcrawl.powers.NoDrawPower;
 
 import basemod.abstracts.CustomCard;
 import conspire.Conspire;
